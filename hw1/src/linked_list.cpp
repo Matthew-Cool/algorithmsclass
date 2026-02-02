113c6394876c077051a37f40621e7489fda3298e
Matthew Wilkinson <mww6121@gmail.com>
2026-02-02 13:00:29 -0600

    some work on hw1

--- hw1/src/linked_list.cpp
+++ hw1/src/linked_list.cpp
@@ -9,19 +9,21 @@ LinkedListNode<T>::LinkedListNode(T value, LinkedListNode<T>* next, LinkedListNo
 
 template<class T>
 LinkedListNode<T>::~LinkedListNode() {
-    this->next = NULL;
+    delete(this->next);
+    //this->next = NULL; // come back to this...
 }
 
 
 template<class T>
 LinkedList<T>::LinkedList() {
      this->root = NULL;
+     this->length = 0;
 }
 
 template<class T>
 LinkedList<T>::~LinkedList() {
     // YOUR CODE HERE
-
+    delete(this->root)
     // END OF YOUR CODE
 }
 
@@ -29,14 +31,41 @@ LinkedList<T>::~LinkedList() {
 template<class T>
 LinkedListNode<T>* LinkedList<T>::insert(T value) {
     // YOUR CODE HERE
+    currentNode = this->root; //possible edge case when length = 0
+    while (!currentNode){
+        if (currentNode->value == value) { //prob can't use this lmao, will get back to it
+            return currentNode; //return pointer to node that already exists
+        } else if (!currentNode->next) {
+            LinkedListNode* node = new LinkedListNode(value, nullptr, &currentNode)
+            this->length++;
+            return &node;
+        } else {
+            currentNode = currentNode->next;
+            std::cout << "switch to next node" << std::endl;
+        }
+    }
 
+    if(currentNode == nullptr){
+        LinkedListNode* node = new LinkedListNode(value, nullptr, &currentNode)
+        this->length++;
+        return &node;
+    }
+
+    std::cout << "oh shit 1" << std::endl;
+    return nullptr;
+    
     // END OF YOUR CODE
 }
 
 template<class T>
 LinkedListNode<T>* LinkedList<T>::find(T value) {
     // YOUR CODE HERE
-    
+    currentNode = this->root;
+    while (!currentNode){
+        break
+    }
+
+    return nullptr;
     // END OF YOUR CODE
 }
 
@@ -50,7 +79,7 @@ LinkedListNode<T>* LinkedList<T>::remove(T value) {
 template<class T>
 int LinkedList<T>::size() {
     // YOUR CODE HERE 
-    
+    return this->length;
     // END OF YOUR CODE
 }
 
