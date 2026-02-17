a53fc3ac93d99491ecb8e296b2b3dd2a9ad74b23
Matthew Wilkinson <mww6121@gmail.com>
2026-02-17 13:10:50 -0600

    init rdfs

--- hw2/src/rdfs.cpp
+++ hw2/src/rdfs.cpp
@@ -2,10 +2,17 @@
 
 void rdfs(Graph &G, int start, int destination) {
     // YOUR CODE HERE
+    G.setVisited(start);
+    //END OF MY CODE HERE
     int numberOfAdjacencyNodes = G.e[start].size();
     LinkedListNode<int> *p = G.e[start].getRoot();
     for (int i = 0; i < numberOfAdjacencyNodes; i += 1, p = p->next) {
         int v = p->value;
         // YOUR CODE HERE
+        if(!G.isVisited(v)){
+            G.setTrace(v, start);
+            rdfs(G, v, destination);
+        }
+        //END OF MY CODE HERE
     }
 }
